1a7b763c30c0304e42327f95e35bb2b421368f7f
David Ankin <daveankin@gmail.com>
2019-10-24 15:38:00 -0400

    new project

--- src/main/java/cloudcmp/davidankin/App.java
+++ src/main/java/cloudcmp/davidankin/App.java
@@ -1,15 +1,25 @@
-package cloudcmp.davidankin;
+// package cloudcmp.davidankin;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-  public static void main( String[] args )
-  {
-    System.out.println( "Hello World!" );
+// /**
+//  * Hello world!
+//  *
+//  */
+// public class App 
+// {
+//   public static void main( String[] args )
+//   {
+//     System.out.println( "Hello World!" );
+//   }
+// }
+
+
+import io.vertx.core.AbstractVerticle;
+public class Server extends AbstractVerticle {
+  public void start() {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8080);
   }
 }
-
-
