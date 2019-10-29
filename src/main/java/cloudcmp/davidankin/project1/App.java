7e588e7fa6ad64ce96673ddcf81d2c4dceee027c
David Ankin <daveankin@gmail.com>
2019-10-29 14:11:36 -0400

    added static handler to resource name

--- src/main/java/cloudcmp/davidankin/project1/App.java
+++ src/main/java/cloudcmp/davidankin/project1/App.java
@@ -4,16 +4,21 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.LoggerHandler;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class App extends AbstractVerticle {
   public void start() {
     Router router = Router.router(vertx);
     router.route().handler(LoggerHandler.create());
+
     router.route("/home").handler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
         .end("Hello from Vert.x!");
     });
+
+    String jarResourceName = "webroot";
+    router.route("/*").handler(StaticHandler.create(jarResourceName));
     router.route().handler(req -> {
       req.fail(404);
     });
